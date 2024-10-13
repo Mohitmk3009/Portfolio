60aadc10ee8b13f3f9102b19bd04afb13925c918
Mohit Kumar <mk0685573@gmail.com>
2024-10-13 19:42:53 +0530

    Update AboutMe.tsx

--- src/app/components/AboutMe.tsx
+++ src/app/components/AboutMe.tsx
@@ -11,7 +11,7 @@ const AboutMe = () => {
                 className="md:w-[300px] md:h-[300px] w-[200px] h-[200px] rounded-full object-cover border-2 border-white" />
             <div id="textabout" className="lg:w-[50%] w-[95%] text-center lg:text-left">
                 <h5 className="uppercase lg:text-3xl text-2xl font-extrabold opacity-60 mb-2">(About me)</h5>
-                <p className="lg:leading-8 leading-6 mb-8 lg:text-xl text-lg text-justify">
+                <p className="lg:leading-8 leading-6 mb-8 lg:text-xl  text-justify">
                 I&#39;m Mohit Kumar, a UI/UX Designer and Front-End Developer with a passion for crafting seamless digital experiences. I love bringing ideas to life, making sure every pixel serves a purpose while creating designs that are both functional and visually captivating. When I&#39;m not immersed in design or code, you&#39;ll find me diving into the world of video games, exploring captivating stories in movies and series, or getting lost in a good book. My goal is to blend creativity and technology to build experiences that people love and remember.
                 </p>
                 <a href="mailto:mohitkumar300902@gmail.com" className="px-6 py-3 border border-white rounded-full text-white text-xs no-underline hover:bg-white hover:text-black">Let&#39;s talk</a>
