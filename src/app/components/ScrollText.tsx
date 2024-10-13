b80ebb71fdf073b9bacbdfd4d4dae33103a8e104
Mohit Kumar <mk0685573@gmail.com>
2024-10-13 15:53:07 +0530

    scrolltext

--- src/app/components/ScrollText.tsx
+++ src/app/components/ScrollText.tsx
@@ -1,27 +1,23 @@
-import React from 'react'
+import React from 'react';
 
-const ScrollText = ({ title }) => {
+interface ScrollTextProps {
+    title: string; // Define the type for title
+}
+
+const ScrollText: React.FC<ScrollTextProps> = ({ title }) => {
     return (
-        <div className='bg-black mt-[-140px] py-[10px]'>
-            <div id="scroller" className='flex-nowrap overflow-x-auto overflow-y-hidden relative z-50'>
-                <div id="scroller-in" className=' flex flex-nowrap lg:gap-40 gap:30'>
-                    {Array(10).fill().map((_, index) => (
-                        <h4 key={index} className="inline-block lg:text-[120px] text-[80px] lg:font-black font-normal   ">
+        <div className="bg-black mt-[-140px] py-[10px]">
+            <div id="scroller" className="flex-nowrap overflow-x-auto overflow-y-hidden relative z-50">
+                <div id="scroller-in" className="flex flex-nowrap lg:gap-40 gap-30">
+                    {Array(10).fill(null).map((_, index) => (
+                        <h4 key={index} className="inline-block lg:text-[120px] text-[80px] lg:font-black font-normal">
                             {title}
                         </h4>
                     ))}
                 </div>
-                {/* <div id="scroller-in">
-                    {Array(5).fill().map((_, index) => (
-                        <h4 key={index} className="">
-                            {title}
-                        </h4>
-                    ))}
-                </div> */}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default ScrollText
-
+export default ScrollText;
