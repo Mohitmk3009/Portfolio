c7a4910be4c926a5a2da7025ab75970d0c18a960
Mohit Kumar <mk0685573@gmail.com>
2024-10-25 00:52:46 +0530

    fix 2

--- src/app/components/Certificate.tsx
+++ src/app/components/Certificate.tsx
@@ -12,18 +12,18 @@ function degToRad(deg: number): number {
   return (deg * Math.PI) / 180;
 }
 
-function Slider(props: React.ComponentProps<'input'>) {
-  return <input type="range" {...props} />;
-}
+// function Slider(props: React.ComponentProps<'input'>) {
+//   return <input type="range" {...props} />;
+// }
 
-function SliderContainer({ title, children }: React.PropsWithChildren<{ title?: string }>) {
-  return (
-    <div className={styles.sliderContainer}>
-      <label>{title}</label>
-      <div className={styles.sliderChildrenWrapper}>{children}</div>
-    </div>
-  );
-}
+// function SliderContainer({ title, children }: React.PropsWithChildren<{ title?: string }>) {
+//   return (
+//     <div className={styles.sliderContainer}>
+//       <label>{title}</label>
+//       <div className={styles.sliderChildrenWrapper}>{children}</div>
+//     </div>
+//   );
+// }
 
 const nextCardProps: TiltCardProps = {
   image: nextImageUrl, // Updated to use image
@@ -99,7 +99,52 @@ export default function Certificate() {
       <div className={styles.yetAnotherWrapper}>
         <div className={styles.tiltCardsWrapper}>{tiltCards}</div>
 
-        
+        {/* <label>
+          Test view
+          <input
+            type="checkbox"
+            checked={showTestView}
+            onChange={() => setShouldEnableTestView((x) => !x)}
+            style={{ marginLeft: '6px' }}
+          />
+        </label>
+        <label
+          className={styles.showBlob}
+          style={{ visibility: showTestView ? 'hidden' : 'visible' }}
+        >
+          Show blob
+          <input
+            type="checkbox"
+            checked={showBlob}
+            onChange={() => setShowBlob((x) => !x)}
+            style={{ marginLeft: '6px' }}
+          />
+        </label>
+        <div
+          className={styles.controls}
+          style={{ visibility: showTestView ? 'visible' : 'hidden' }}
+        >
+          <SliderContainer title="Angle:">
+            <span>{angle}°</span>
+            <Slider
+              min="0"
+              max="180"
+              value={angle}
+              onChange={(ev) => setAngle(Number.parseInt(ev.target.value, 10))}
+            />
+          </SliderContainer>
+          <SliderContainer title="Direction of axis of rotation:">
+            <span>{directionAngle}°</span>
+            <Slider
+              min="0"
+              max="360"
+              value={directionAngle}
+              onChange={(ev) =>
+                setDirectionAngle(Number.parseInt(ev.target.value, 10))
+              }
+            />
+          </SliderContainer>
+        </div> */}
       </div>
     </div>
   );
